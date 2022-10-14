5afc6a26cfa0de45e3416dbf895c605ecb2d0b64
SPYDR <spydr.xyz@gmail.com>
2022-10-15 03:32:40 +0600

    Added Survey List templates

--- os_app/urls.py
+++ os_app/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 app_name = 'os_app'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('survey-list/', views.surveylist, name='surveylist')
 ]
--- os_app/views.py
+++ os_app/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'os_app/index.html')
+
+
+def surveylist(request):
+    return render(request, 'os_app/surveylist.html')
